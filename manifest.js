208775de2b509830e75235aeb5eff49d42bb48da
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2024-03-09 13:12:32 -0300

    1234

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+    "background_color": "#fefefe",
+    "dir": "ltr",
+    "display": "standalone",
+    "name": "Dengue Match",
+    "orientation": "any",
+    "scope": "/",
+    "short_name": "Dengue Match",
+    "start_url": "/",
+    "theme_color": "#ffffff",
+    "icons": [
+      {
+        "src": "./images/brain.png",
+        "type": "image/x-icon"
+      }
+    ],
+    "description": "Aplicativo para faculdade",
+    "lang": "pt",
+    "categories": [
+      "education",
+      "games"
+    ]
+  }
